7793e911de5575dbe53ec44b33487cd10236feab
unknown <sairam.narina@gmail.com>
2020-07-26 19:49:43 +0530

    Updates

---
+++ precache-manifest.cdde0d0b60d20dd82af3e8a7abbf1cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a550be0f95705b7297840f77be7e199d",
+    "url": "/React-SearchFood/index.html"
+  },
+  {
+    "revision": "2f894148ab09047b23f5",
+    "url": "/React-SearchFood/static/css/main.7f6ab7af.chunk.css"
+  },
+  {
+    "revision": "027b6ed4a6742e9f4dc2",
+    "url": "/React-SearchFood/static/js/2.d0f7d564.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-SearchFood/static/js/2.d0f7d564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f894148ab09047b23f5",
+    "url": "/React-SearchFood/static/js/main.93af29f0.chunk.js"
+  },
+  {
+    "revision": "661806a994d90ae46356",
+    "url": "/React-SearchFood/static/js/runtime-main.769740f9.js"
+  }
+]);
